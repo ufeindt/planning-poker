16fb3da09850dbef989b769d4416a51d8a8dd2e8
Ulrich Feindt <ufeindt@gmail.com>
2021-10-05 19:47:49 +0200

    Setup frontend, backend, and docker
    The following components were set up:
    - Angular frontend (with routing, `--host 0.0.0.0` added to `npm start`)
    - FastAPI HelloWorld
    - Dockerfiles for both
    - docker-compose.yml file to run the app, including a MongoDB isntance

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:4200",
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
